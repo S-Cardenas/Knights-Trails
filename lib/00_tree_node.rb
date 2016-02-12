d34af96b8368257b9ed5b7b48924497afe52f552
GraahmAndStefan <appacademy@Apps-Mac-mini-54.local>
2016-02-12 13:52:39 -0500

    Add add_child and remove_child methods

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   attr_accessor :value, :parent, :children
   def initialize(value)
@@ -7,16 +9,23 @@ class PolyTreeNode
   end
 
 
-  def children
-    children
-  end
 
   def parent=(parent)
+    @parent.children.delete(self) if !@parent.nil?
+
+    parent.children << self if !parent.nil? && !parent.children.include?(self)
     @parent = parent
-    parents.children << self
   end
 
+  def add_child(child)
+    children << child
+    child.parent = self
+  end
 
-
+  def remove_child(child)
+    raise "Not a child" if !children.include?(child)
+    children.delete(child)
+    child.parent = nil
+  end
 
 end
